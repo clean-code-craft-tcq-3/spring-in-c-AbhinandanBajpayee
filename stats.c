8da599e032dc095b2e4c5effeb14116bd85db24c
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-02-27 11:21:11 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,6 +5,21 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    int loopCntr_u16 = 0;
+    for(loopCntr_u16 = 0; loopCntr_u16 < setlength; loopCntr_u16++)
+    {
+      if(numberset[loopCntr_u16] > numberset[loopCntr_u16 + 1])
+      {
+          s.max = numberset[loopCntr_u16];
+      }
+      else
+      {
+          s.min = numberset[loopCntr_u16];
+      }
+    }
+    s.average = (s.max + s.min)/2;
+    
+    return s;
 }
 
 int emailAlertCallCount = 0;
