e22120ea1efe239226f7d7a8ff7ee2977a512f28
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-02-27 14:54:04 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -49,7 +49,7 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 {
    if(computedStats.max > maxThreshold)
    {  
-         emailAlertCallCount = 1;
+        emailAlertCallCount = 1;
          ledAlertCallCount = 1;
    }
 }
