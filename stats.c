995c7feff82ee26e7f142b31482a1ba4dcb110c3
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-02-28 19:30:50 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -3,8 +3,14 @@
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
-alerter_funcptr emailAlerter = 0;
-alerter_funcptr ledAlerter = 0;
+alerter_funcptr emailAlerter()
+{
+  return 1;
+};
+alerter_funcptr ledAlerter()
+{
+  return 1;
+};
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
@@ -17,42 +23,42 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     float min = 0 ;
     if(numberset != 0)
     {
-    max = *(numberset+0);
-    min = *(numberset+0);
-    for(loopCntr_u16 = 0; loopCntr_u16 < setlength; loopCntr_u16++)
-    {
-      if(*(numberset+loopCntr_u16) > max)
-      {
-          max = *(numberset+loopCntr_u16);
-      }
-      if(*(numberset+loopCntr_u16) < min)
-      {
-          min = *(numberset+loopCntr_u16);
-      }
-        sum = sum + *(numberset+loopCntr_u16);
-    }
-    if( setlength != 0 )
-    {
-     s.average = sum/setlength;
-     s.max = max;
-     s.min = min;
-    }
+       max = *(numberset+0);
+       min = *(numberset+0);
+       for(loopCntr_u16 = 0; loopCntr_u16 < setlength; loopCntr_u16++)
+       {
+         if(*(numberset+loopCntr_u16) > max)
+         {
+             max = *(numberset+loopCntr_u16);
+         }
+         if(*(numberset+loopCntr_u16) < min)
+         {
+             min = *(numberset+loopCntr_u16);
+         }
+          sum = sum + *(numberset+loopCntr_u16);
+       }
+      s.average = sum/setlength;
     }
     else
     {
      s.average = 0;
      s.max = 0;
      s.min = 0;
-    }
-    
+    }    
     return s;
 }
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
    if(computedStats.max > maxThreshold)
-   {  
-        emailAlertCallCount = 1;
-         ledAlertCallCount = 1;
+   {
+       if( 1 == alerters[0])
+       {
+         emailAlertCallCount = 1;
+       }
+       if( 1 == alerters[1])
+       {
+          ledAlertCallCount = 1;
+       }
    }
 }
