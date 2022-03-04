967cae217eca42cecd0696ebe05f30510cb020ac
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-03-04 12:13:11 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -35,7 +35,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     }
     else
     {
-     s.average = NAN;
+     s.average = 0;
      s.max = 0;
      s.min = 0;
     }    
